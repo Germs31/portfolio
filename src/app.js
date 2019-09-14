b42756b116fa7886cd295a715de7d0db6bf20a0e
German Nunez <germannunez@Germans-MacBook-Pro.local>
2019-09-13 21:14:44 -0700

    sliding nav on scroll

--- src/app.js
+++ src/app.js
@@ -4,3 +4,13 @@ let navBarToggle = document.getElementById('glasses-hamburger');
 navBarToggle.addEventListener('click',  () => {
     mainNav.classList.toggle('active');
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.querySelector(".navbar").style.top = "0";
+  } else {
+    document.querySelector(".navbar").style.top = "-100px";
+  }
+}
